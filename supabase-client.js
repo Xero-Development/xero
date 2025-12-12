1fd3d0a6c621e2354d51b4de8d706d16059defb2
Popout Rage <dawnshafer05@gmail.com>
2025-12-11 23:05:01 -0500

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,3 @@
-import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 window.sb  = createClient('https://qopgwxlrdqjyamerllwz.supabase.co', 'sb_publishable_HYgmuRFRreMN0le5UX26PA_Nkbul_ae')
